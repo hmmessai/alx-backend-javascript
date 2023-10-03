0ac518c7d323c7e1088e9cfab4bafedbcb161b99
Messai <messai@github.com>
2023-10-03 12:51:28 +0300

    4 n 5

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,18 @@
 export default function updateStudentGradeByCity(list, city, newGrades) {
-  if (!(list)) {
-    
+  if (!(list instanceof Array)) {
+    return [];
   }
 
-  
+  const defaultGrade = { grade: 'N/A' };
+
+  return list
+    .filter((student) => student.location === city)
+    .map((student) => ({
+      id: student.id,
+      firstName: student.firstName,
+      location: student.location,
+      grade: (newGrades
+        .filter((grade) => grade.studentId === student.id)
+        .pop() || defaultGrade).grade,
+    }));
 }
---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+
+  const dv = new DataView(buffer);
+
+  dv.setInt8(position, value);
+
+  return dv;
+}
